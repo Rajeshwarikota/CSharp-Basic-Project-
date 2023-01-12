1f87b2c8292115e10e48e4f856b23ebdf04504e1
Rajeshwarikota <kotarajeswari003.com>
2023-01-12 08:28:05 +0530

    [Rajeshwari] Baic CSharp Project

---
+++ Csharp Basic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Csharp Basic Project");
+            Console.ReadLine();
+        }
+    }
+}

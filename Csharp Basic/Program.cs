7940ad7d698cc8a0ea5416ed80be8ad358e1aaae
Rajeshwarikota <kotarajeswari003.com>
2023-01-13 22:35:27 +0530

    [Rajeshwari] Add SwitchCase Concept

--- Csharp Basic/Program.cs
+++ Csharp Basic/Program.cs
@@ -11,46 +11,51 @@ namespace Csharp_Basic
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Csharp Basic Project");
-            
-            Console.WriteLine("please enter the first number");
-            int first=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("please enter the second number");
-            int second = Convert.ToInt32(Console.ReadLine());
 
-            Calculator Calculator1 = new Calculator(first,second);
-            Calculator1.Addition();
-            Calculator1.substraction();
+            /// Console.WriteLine("please enter the first number");
+            ///int first=Convert.ToInt32(Console.ReadLine());
+            ///Console.WriteLine("please enter the second number");
+            ///int second = Convert.ToInt32(Console.ReadLine());
+
+            ///Calculator Calculator1 = new Calculator(first,second);
+            ///Calculator1.Addition();
+            ///Calculator1.substraction();
 
 
-            Console.WriteLine("please enter the first number");
-            int third = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("please enter the second number");
-            int fourth = Convert.ToInt32(Console.ReadLine());
+            ///Console.WriteLine("please enter the first number");
+            ///int third = Convert.ToInt32(Console.ReadLine());
+            ///Console.WriteLine("please enter the second number");
+            ///int fourth = Convert.ToInt32(Console.ReadLine());
 
-            Calculator Calculator2 = new Calculator(third,fourth);
-            Calculator2.Addition();
-            Calculator2.substraction();
+            /// Calculator Calculator2 = new Calculator(third,fourth);
+            ///Calculator2.Addition();
+            ///   Calculator2.substraction();
 
-            Console.WriteLine("please enter the first number");
-            int fifth = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("please enter the second number");
-            int sixth = Convert.ToInt32(Console.ReadLine());
+            ///Console.WriteLine("please enter the first number");
+            ///int fifth = Convert.ToInt32(Console.ReadLine());
+            ///Console.WriteLine("please enter the second number");
+            ///int sixth = Convert.ToInt32(Console.ReadLine());
 
-            Calculator Calculator3 = new Calculator(fifth, sixth);
-            Calculator3.Addition();
-            Calculator3.substraction();
-            Calculator3.Multiplication();
+            ///Calculator Calculator3 = new Calculator(fifth, sixth);
+            ///  Calculator3.Addition();
+            ///Calculator3.substraction();
+            ///Calculator3.Multiplication();
 
-            Console.WriteLine("please enter the first number");
-            int seventh = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("please enter the second number");
-            int eighth = Convert.ToInt32(Console.ReadLine());
+            ///Console.WriteLine("please enter the first number");
+            ///int seventh = Convert.ToInt32(Console.ReadLine());
+            ///Console.WriteLine("please enter the second number");
+            ///int eighth = Convert.ToInt32(Console.ReadLine());
 
-            Calculator Calculator4 = new Calculator(seventh, eighth);
-            Calculator4.Addition();
-            Calculator4.substraction();
-            Calculator4.Multiplication(); 
-            Calculator4.Division();
+            ///Calculator Calculator4 = new Calculator(seventh, eighth);
+            ///Calculator4.Addition();
+            ///Calculator4.substraction();
+            ///Calculator4.Multiplication(); 
+            ///Calculator4.Division();
+
+
+            Console.WriteLine("please choose a day 'from 1 to 7'");
+            int day = Convert.ToInt32(Console.ReadLine());
+            WeekDays.FindDayWhichFallsOnWeek(day);
             Console.ReadLine();
 
         }
---
+++ Csharp Basic/WeekDays.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Basic
+{
+    internal class WeekDays
+    {
+        public static void FindDayWhichFallsOnWeek(int dayNumber)
+        {
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("It's a Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("It's a Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("It's a Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("It's a Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("It's a Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("It's a Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("It's a Saturday");
+                    break;
+                default:
+                    Console.WriteLine("please choose number between 1 to 7");
+                    break;
+
+            }
+        }
+    }
+}
